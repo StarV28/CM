5a5c38fb32de71e20ea3e7e78dc10f049b23d4e8
Vlad Starovoit <vladimirstar80@gmail.com>
2026-02-26 13:22:33 +0100

    stoping job cron

--- backend/api-data/src/v1/job/cron/start.cron.ts
+++ backend/api-data/src/v1/job/cron/start.cron.ts
@@ -1,22 +1,22 @@
-import cron from "node-cron";
-import { jobs } from "./jobs.cron.js";
-import { runJob } from "./runJob.cron.js";
+// import cron from "node-cron";
+// import { jobs } from "./jobs.cron.js";
+// import { runJob } from "./runJob.cron.js";
 
-export async function startCronScheduler() {
-  console.log("[CRON] Warmup:first start all task...");
+// export async function startCronScheduler() {
+//   console.log("[CRON] Warmup:first start all task...");
 
-  for (const job of jobs) {
-    try {
-      await runJob(job);
-    } catch (err) {
-      console.error(`[CRON] Warmup ${job.name} failed:`, err);
-    }
-  }
+//   for (const job of jobs) {
+//     try {
+//       await runJob(job);
+//     } catch (err) {
+//       console.error(`[CRON] Warmup ${job.name} failed:`, err);
+//     }
+//   }
 
-  console.log("[CRON] Warmup finish. launch on schedule...");
+//   console.log("[CRON] Warmup finish. launch on schedule...");
 
-  for (const job of jobs) {
-    cron.schedule(job.schedule, () => runJob(job));
-    console.log(`[CRON] ${job.name} → ${job.schedule}`);
-  }
-}
+//   for (const job of jobs) {
+//     cron.schedule(job.schedule, () => runJob(job));
+//     console.log(`[CRON] ${job.name} → ${job.schedule}`);
+//   }
+// }
