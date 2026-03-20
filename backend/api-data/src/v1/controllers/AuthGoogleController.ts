5bf7b732c6d7ee3bd628cabd0661235cc1e74ff8
Vlad Starovoit <vladimirstar80@gmail.com>
2026-03-20 15:45:32 +0100

    change path AuthGoogleControler

--- backend/api-data/src/v1/controllers/AuthGoogleController.ts
+++ backend/api-data/src/v1/controllers/AuthGoogleController.ts
@@ -9,13 +9,13 @@ import FavoriteService from "../modules/favoriteService.js";
 const client = new OAuth2Client(
   config.authGoogle?.clientId,
   config.authGoogle?.clientSecret,
-  `${config.url.api_url}/auth/google/callback`
+  `${config.url.api_url}/auth/google/callback`,
 );
 
 export const googleAuthRedirect = (
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   try {
     const locale = req.query.locale?.toString();
@@ -37,7 +37,7 @@ export const googleAuthRedirect = (
 export const googleAuthCallback = async (
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   try {
     const codeRaw = req.query.code;
@@ -95,7 +95,7 @@ export const googleAuthCallback = async (
     const token = prepareToken({ id: user.id, email: user.email });
 
     return res.redirect(
-      `${config.url.frontend_url}/${locale}/auth/login?token=${token}`
+      `${config.url.frontend_url}/${locale}/auth-login?token=${token}`,
     );
   } catch (error) {
     console.error("Google OAuth Error:", (error as Error)?.message);
@@ -106,7 +106,7 @@ export const googleAuthCallback = async (
 export const googleAnswerMe = async (
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   try {
     const authHeader = req.headers.authorization;
@@ -148,13 +148,6 @@ export const googleAnswerMe = async (
       locale: userData.locale ?? null,
       favorites: favorites ?? null,
     });
-    // res.json({
-    //   id: user.id,
-    //   email: user.email,
-    //   name: user.name ?? null,
-    //   locale: user.locale ?? null,
-    //   favorites: favorites ?? null,
-    // });
   } catch (error) {
     console.error(error);
     next(error);
