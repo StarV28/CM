82c104fb2b498422f7c398e091c8aa9bdc6f6ad5
Vlad Starovoit <vladimirstar80@gmail.com>
2026-04-24 18:50:52 +0200

    techical commit delete console

--- backend/market-data/src/v1/infrastructure/integrations/exServices/exchanges/okx/okx.mapping.ts
+++ backend/market-data/src/v1/infrastructure/integrations/exServices/exchanges/okx/okx.mapping.ts
@@ -3,13 +3,13 @@ import type { OkxSymbol, OkxInfo } from "../../types/exchangesMapp.js";
 
 export async function getOkxSpotUsdtMarkets() {
   const { data } = await axios.get<OkxInfo>(
-    "https://www.okx.com/api/v5/public/instruments?instType=SPOT"
+    "https://www.okx.com/api/v5/public/instruments?instType=SPOT",
   );
 
   return data.data
     .filter(
       (s: OkxSymbol) =>
-        s.instType === "SPOT" && s.state === "live" && s.quoteCcy === "USDT"
+        s.instType === "SPOT" && s.state === "live" && s.quoteCcy === "USDT",
     )
     .map((s: OkxSymbol) => ({
       exchange: "okx" as const,
