d13bf4d1d0cce4d22e6476ecaea9ef44fa8962dd
Vlad Starovoit <vladimirstar80@gmail.com>
2026-04-26 18:24:42 +0200

    fixed connect_Server_Redis

--- backend/market-data/db/connect_Server_Redis.ts
+++ backend/market-data/db/connect_Server_Redis.ts
@@ -1,15 +1,17 @@
 // <reference types="ioredis" />
 import { Redis } from "ioredis";
 import config from "../config/index.js";
+//---------------------------------------//
+const urlRedis = config.url.redis_url;
+if (!urlRedis) {
+  throw new Error("REDIS_URL is not defined");
+}
 
-export const redis_server = new Redis(
-  config.url.redis_url ?? "redis://localhost:6379",
-  {
-    lazyConnect: false,
-    connectTimeout: 5000,
-    maxRetriesPerRequest: 1,
-  },
-);
+export const redis_server = new Redis(urlRedis, {
+  lazyConnect: false,
+  connectTimeout: 5000,
+  maxRetriesPerRequest: 3,
+});
 
 redis_server.on("connect", () => console.log("✅ Redis connected"));
 redis_server.on("error", (err: Error) => console.error("Redis error:", err));
